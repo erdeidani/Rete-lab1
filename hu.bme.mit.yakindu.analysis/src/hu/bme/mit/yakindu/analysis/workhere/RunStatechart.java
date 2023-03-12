60cc871f705009f7b513674a675740b044fd2495
Daniel Erdei <100084924+erdeidani@users.noreply.github.com>
2023-03-12 19:40:26 +0100

    3.6

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -1,9 +1,10 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
 import java.io.IOException;
+import java.util.Scanner;
 
-// import hu.bme.mit.yakindu.analysis.RuntimeService;
-// import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 
@@ -12,20 +13,48 @@ import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 public class RunStatechart {
 	
 	public static void main(String[] args) throws IOException {
-//		ExampleStatemachine s = new ExampleStatemachine();
-//		s.setTimer(new TimerService());
-//		RuntimeService.getInstance().registerStatemachine(s, 200);
-//		s.init();
-//		s.enter();
-//		s.runCycle();
-//		print(s);
-//		s.raiseStart();
-//		s.runCycle();
-//		System.in.read();
-//		s.raiseWhite();
-//		s.runCycle();
-//		print(s);
-//		System.exit(0);
+		ExampleStatemachine s = new ExampleStatemachine();
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+	
+		s.init();
+		s.enter();
+		
+		s.runCycle();
+		print(s);
+		
+		Scanner sc; 
+		while(true) {
+			sc = new Scanner(System.in);
+			String input=sc.nextLine();
+			
+			if(input.equals("start")) {
+				s.raiseStart();
+				s.runCycle();
+				print(s);
+			}
+			
+			if(input.equals("white")) {
+				s.raiseWhite();
+				s.runCycle();
+				print(s);
+			}
+			
+			if(input.equals("black")) {
+				s.raiseBlack();
+				s.runCycle();
+				print(s);
+			}
+			
+			if(input.equals("exit")) {
+				print(s);
+				System.exit(0);
+			}
+		}
+		
+		
+		
+		
 	}
 
 	public static void print(IExampleStatemachine s) {
